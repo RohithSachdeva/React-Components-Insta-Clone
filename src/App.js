bd0bc44704a874d597073c5833962eeaccf4f0a8
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-17 13:25:26 -0700

    Start of project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 
