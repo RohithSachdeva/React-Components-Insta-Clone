d584c31de9e801884ddc1d1863db773bf01a7506
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-18 12:14:48 -0700

    Trying to figure out some hook issue.. gonna redo a part

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PostsPage from "./components/PostsContainer/PostsPage";
 import SearchBar from "./components/SearchBar/SearchBarContainer";
+
+
 import "./App.css";
+
 // import the PostsPage and SearchBar and add them to the App
 
 
@@ -15,6 +18,8 @@ const App = () => {
 
   return (
     <div className="App">
+      <SearchBar/>
+      <PostsPage/>
       {/* Add imported components here to render them */}
     </div>
   );
